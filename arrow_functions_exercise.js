1b7c7a8907c0d9b8548a97adf817b163cfc96c4d
Mike <wattonlaw@gmail.com>
2021-11-15 13:21:37 -0600

    new commit test

--- arrow_functions_exercise.js
+++ arrow_functions_exercise.js
@@ -1,3 +1,5 @@
 const double = arr => arr.map(val => val * 2);
 
-const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(sqaure => sqaure % 2 === 0)const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(sqaure => sqaure % 2 === 0)
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(sqaure => sqaure % 2 === 0)
+
+// testing
